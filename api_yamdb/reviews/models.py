38c261c8575e474349f28cc040ab577c253ea605
AlexandrLobachev <alexandrlobachev@gmail.com>
2023-12-11 11:09:00 +0300

    Созданы модели ктегории, жанра и произведения

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,70 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=256)
+    slug = models.SlugField(
+        'Идентификатор',
+        unique=True,
+        help_text=('Идентификатор страницы для URL; '
+                   'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=256)
+    slug = models.SlugField(
+        'Идентификатор',
+        unique=True,
+        help_text=('Идентификатор страницы для URL; '
+                   'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    )
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название произведения', max_length=256)
+    year = models.PositiveSmallIntegerField(
+        'Год выпуска',
+        validators=[MaxValueValidator(
+            datetime.date.today().year,
+            message='Произведение не может быть из будущего!'
+        )]
+    )
+    description = models.TextField(
+        'Описание',
+        null=True,
+        blank=True,
+    )
+    genre = models.ManyToManyField(Genre, verbose_name='Жанр')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Категория',
+    )
+
+    class Meta:
+        default_related_name = 'titles'
+        verbose_name = 'произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
