4f85e45bf3f8e4f41fb7e37e6fd04922c72b4dd3
AlexandrLobachev <146027535+AlexandrLobachev@users.noreply.github.com>
2023-12-11 22:36:53 +0300

    Реальзованы вьюшки и эндпойнты для категорий и жанров.

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,19 @@
+from rest_framework import routers
+
+from django.urls import include, path
+
+from .views import (
+    CategoryViewSet,
+    GenreViewSet,
+)
+
+
+app_name = 'api'
+
+router_review_v1 = routers.DefaultRouter()
+router_review_v1.register('categories', CategoryViewSet)
+router_review_v1.register('genres', GenreViewSet)
+
+urlpatterns = [
+    path('v1/', include(router_review_v1.urls)),
+]
