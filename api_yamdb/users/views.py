a41f69168b6febbf214677dcc1801b3d58d2eac4
Екатерина Карпова <karpova.cestmoi@gmail.com>
2023-12-12 10:15:14 +0300

    USER-2: ручка регистрации /auth/signup/

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignupSerializer
+from .utils import send_confirmation_code_to_email
 
-# Create your views here.
+User = get_user_model()
+
+
+class SignupView(APIView):
+    """Регистрация пользователя.
+
+    Если пользователь уже существует, отправляем письмо с кодом подтверждения.
+    Если пользователь не существует, валидируем входящие данные,
+    сохраняем пользователя в базе и отправляем письмо.
+    """
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        email = request.data.get('email')
+        if User.objects.filter(username=request.data.get('username'),
+                               email=email).exists():
+            send_confirmation_code_to_email(email)
+            return Response('Код подтверждения отправлен',
+                            status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        email = serializer.validated_data.get('email')
+        send_confirmation_code_to_email(email)
+        return Response(serializer.data, status=status.HTTP_200_OK)
