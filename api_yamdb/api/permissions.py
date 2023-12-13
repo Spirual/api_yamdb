1c5a7f280bc5bf88c203e3d35af6d1111ec9414c
Екатерина Карпова <karpova.cestmoi@gmail.com>
2023-12-13 11:27:02 +0300

    USER-6: Права доступа

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,48 @@
+from rest_framework.permissions import (
+    SAFE_METHODS, BasePermission
+)
+
+
+class IsAdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        """Проверяем авторизацию пользователя.
+
+        Если метод в запросе является безопасным,
+        независимо от наличия авторизации возвращаем True,
+        для остальных методов проверяем, что юзер из запроса является админом.
+        """
+        return request.method in SAFE_METHODS or request.user.is_admin
+
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        """Проверяем, что пользователь является админом"""
+        return request.user.is_admin
+
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        """Проверяем авторизацию пользователя.
+
+        Если метод в запросе является безопасным,
+        независимо от наличия авторизации возвращаем True,
+        для остальных методов проверяем, что юзер из запроса авторизован
+        """
+        return request.method in SAFE_METHODS or request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        """Проверяем права при попытке редактирования или удаления объекта.
+
+        Если метод в запросе является безопасным, то возвращаем True,
+        для всех других методов проверяем, что юзер из запроса является
+        автором/модератором/админом/суперюзером в случае попытки
+        редактирования/удаление сущности, для которой отправлен запрос.
+        """
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin
+                )
