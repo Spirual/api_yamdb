5a2bb30561829c3cc784916ba8f129211de636fa
Spirual <spiuaal@gmail.com>
2023-12-16 08:54:48 +0300

    Исправлены недочеты после 1-го ревью.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -96,16 +96,12 @@ class ReviewSerializer(ModelSerializer):
         fields = ('id', 'text', 'author', 'score', 'pub_date')
         model = Review
 
-    def validate_score(self, value):
-        if not 1 <= value <= 10:
-            raise serializers.ValidationError('Оценка должна быть от 1 до 10!')
-        return value
 
     def validate(self, data):
-        title_id = self.context['view'].kwargs.get('title_id')
         request = self.context['request']
-        title = get_object_or_404(Title, id=title_id)
         if request.method == 'POST':
+            title_id = self.context['view'].kwargs.get('title_id')
+            title = get_object_or_404(Title, id=title_id)
             if Review.objects.filter(
                 author=request.user, title=title
             ).exists():
